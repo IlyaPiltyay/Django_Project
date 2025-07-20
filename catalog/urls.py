bf57546581340d09bbf5f849845f5718901cba0d
Ilya <ilya.piltyay@gmail.com>
2025-07-20 22:29:14 +0300

    add dotenv again

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('catalog/', CatalogView.as_view(), name='catalog'),
+
     path('products/', ProductListView.as_view(), name='product_list'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('products/create/', ProductCreateView.as_view(), name='product_create'),
